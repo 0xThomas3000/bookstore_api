6513357a9eeba79f2aa18e64919a20f09330374c
Thomas Ngo <tuanit168@gmail.com>
2024-06-09 23:14:35 +0700

    Fix unit-test at Business layer for AddBook

--- features/book/business/add_book_test.go
+++ features/book/business/add_book_test.go
@@ -2,19 +2,41 @@ package bookbusiness
 
 import (
 	"context"
-	"errors"
 	"testing"
 
-	"github.com/0xThomas3000/bookstore_api/core"
 	bookentity "github.com/0xThomas3000/bookstore_api/features/book/entities"
 )
 
 type mockAddStore struct{}
 
 func (mockAddStore) Add(context context.Context, data *bookentity.BookAdd) error {
-	if data.Title == "Harry Potter" {
-		return core.ErrDB(errors.New("invalid input data"))
+	if data.Title == "" {
+		return bookentity.ErrTitleIsEmpty
 	}
+	if data.Title == "Harry Potter" && data.Author == "" {
+		return bookentity.ErrAuthorIsEmpty
+	}
+	if data.Title == "Harry Potter" && data.Author == "J. K. Rowling" && data.PublishedDate == "1997/06/26" {
+		return bookentity.ErrPublishedDateInvalid
+	}
+	if data.Title == "Harry Potter" && data.Author == "J. K. Rowling" && data.PublishedDate == "1997-06-26" && data.Isbn == "123456" {
+		return bookentity.ErrIsbnInvalid
+	}
+	if data.Title == "Harry Potter" && data.Author == "J. K. Rowling" &&
+		data.PublishedDate == "1997-06-26" && data.Isbn == "1234567890123" && data.NumberOfPages == -3 {
+		return bookentity.ErrNumberInvalid
+	}
+	if data.Title == "Harry Potter" && data.Author == "J. K. Rowling" &&
+		data.PublishedDate == "1997-06-26" && data.Isbn == "1234567890123" &&
+		data.NumberOfPages == 3 && data.Language == "" {
+		return bookentity.ErrLanguageIsEmpty
+	}
+	if data.Title == "Harry Potter" && data.Author == "J. K. Rowling" &&
+		data.PublishedDate == "1997-06-26" && data.Isbn == "1234567890123" &&
+		data.NumberOfPages == 3 && data.Language == "English" {
+		return nil
+	}
+
 	data.Id = 10
 	return nil
 }
@@ -25,21 +47,21 @@ func TestNewAddBookBusiness(t *testing.T) {
 	// Case 1
 	dataTest := bookentity.BookAdd{Title: ""}
 	err := business.AddBook(context.Background(), &dataTest)
-	if err == nil || err.Error() != "invalid input data" {
+	if err == nil || err.Error() != bookentity.ErrTitleIsEmpty.Error() {
 		t.Errorf("Failed")
 	}
 
 	// Case 2
 	dataTest = bookentity.BookAdd{Title: "Harry Potter", Author: ""}
 	err = business.AddBook(context.Background(), &dataTest)
-	if err == nil || err.Error() != "invalid input data" {
+	if err == nil || err.Error() != bookentity.ErrAuthorIsEmpty.Error() {
 		t.Errorf("Failed")
 	}
 
 	// Case 3
 	dataTest = bookentity.BookAdd{Title: "Harry Potter", Author: "J. K. Rowling", PublishedDate: "1997/06/26"}
 	err = business.AddBook(context.Background(), &dataTest)
-	if err == nil || err.Error() != "invalid input data" {
+	if err == nil || err.Error() != bookentity.ErrPublishedDateInvalid.Error() {
 		t.Errorf("Failed")
 	}
 
@@ -47,7 +69,7 @@ func TestNewAddBookBusiness(t *testing.T) {
 	dataTest = bookentity.BookAdd{Title: "Harry Potter", Author: "J. K. Rowling", PublishedDate: "1997-06-26",
 		Isbn: "123456"}
 	err = business.AddBook(context.Background(), &dataTest)
-	if err == nil || err.Error() != "invalid input data" {
+	if err == nil || err.Error() != bookentity.ErrIsbnInvalid.Error() {
 		t.Errorf("Failed")
 	}
 
@@ -55,7 +77,7 @@ func TestNewAddBookBusiness(t *testing.T) {
 	dataTest = bookentity.BookAdd{Title: "Harry Potter", Author: "J. K. Rowling", PublishedDate: "1997-06-26",
 		Isbn: "1234567890123", NumberOfPages: -3}
 	err = business.AddBook(context.Background(), &dataTest)
-	if err == nil || err.Error() != "invalid input data" {
+	if err == nil || err.Error() != bookentity.ErrNumberInvalid.Error() {
 		t.Errorf("Failed")
 	}
 
@@ -63,7 +85,7 @@ func TestNewAddBookBusiness(t *testing.T) {
 	dataTest = bookentity.BookAdd{Title: "Harry Potter", Author: "J. K. Rowling", PublishedDate: "1997-06-26",
 		Isbn: "1234567890123", NumberOfPages: 3, Language: ""}
 	err = business.AddBook(context.Background(), &dataTest)
-	if err == nil || err.Error() != "invalid input data" {
+	if err == nil || err.Error() != bookentity.ErrLanguageIsEmpty.Error() {
 		t.Errorf("Failed")
 	}
 
@@ -71,7 +93,7 @@ func TestNewAddBookBusiness(t *testing.T) {
 	dataTest = bookentity.BookAdd{Title: "Harry Potter", Author: "J. K. Rowling", PublishedDate: "1997-06-26",
 		Isbn: "1234567890123", NumberOfPages: 3, Language: "English"}
 	err = business.AddBook(context.Background(), &dataTest)
-	if err == nil || err.Error() != "invalid input data" {
+	if err != nil {
 		t.Errorf("Failed")
 	}
 }
